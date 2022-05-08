084390b477133a8a3bb73b0c5132200821909861
jinho <rowkaxl@naver.com>
2022-05-08 15:18:11 +0900

    initialize

---
+++ AppstoreTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppstoreTransition
+//
+//  Created by jinho on 2022/05/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
